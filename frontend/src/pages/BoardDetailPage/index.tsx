f841efe03a5aff90f74d8fda19dba39537a1cf4a
이상휘 <sanghwi47@naver.com>
2024-09-11 14:09:35 +0900

    Chore: 프로젝트 페이지, 폴더구조 작성 /S11P12B201-114

---
+++ frontend/src/pages/BoardDetailPage/index.tsx
@@ -0,0 +1,5 @@
+const BoardDetailPage = () => {
+  return <div></div>;
+};
+
+export default BoardDetailPage;
---
+++ frontend/src/pages/BoardWritePage/index.tsx
@@ -0,0 +1,5 @@
+const BoardWritePage = () => {
+  return <div></div>;
+};
+
+export default BoardWritePage;
