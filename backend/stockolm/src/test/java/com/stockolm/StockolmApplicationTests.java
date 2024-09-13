157147a23962d07f88cb8e5b55a701ebc4bf8ad4
Yungsix <dbstjr9898@naver.com>
2024-09-13 12:32:34 +0900

    refactor: test 파일 설정 변경

--- backend/stockolm/src/test/java/com/stockolm/StockolmApplicationTests.java
+++ backend/stockolm/src/test/java/com/stockolm/StockolmApplicationTests.java
@@ -3,7 +3,7 @@ package com.stockolm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Test.class)
 class StockolmApplicationTests {
 
     @Test
