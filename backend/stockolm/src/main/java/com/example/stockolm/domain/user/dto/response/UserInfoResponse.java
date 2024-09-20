c45fc90c2941e44323863b05ba301a3d254b3610
Yungsix <dbstjr9898@naver.com>
2024-09-20 13:14:59 +0900

    feat : 내 정보 조회 api 개발

---
+++ backend/stockolm/src/main/java/com/example/stockolm/domain/user/dto/response/UserInfoResponse.java
@@ -0,0 +1,13 @@
+package com.example.stockolm.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoResponse {
+
+    private String userNickName;
+    private String userImagePath;
+
+}
