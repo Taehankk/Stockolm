ece6d06f339de33e31f99afe5b726c5bc5985ab2
김민호 <oilpott12@naver.com>
2024-09-23 17:14:46 +0900

    feat: 바 차트 엘리먼트 구현
    props로 값, 색상 필수, 라벨 선택

---
+++ frontend/src/components/elements/BarChart/index.tsx
@@ -0,0 +1,114 @@
+import ReactApexChart from "react-apexcharts";
+
+interface BarChartProps {
+  value: number;
+  color: string;
+  children?: string;
+}
+
+const BarChart = ({
+    value = 40,
+    color="#ffd400",
+    children ="반도체",
+}: BarChartProps) => {
+  const validValue = Math.round(value) || 0;
+
+  const optionsLight = {
+    chart: {
+      type: 'bar' as const,
+      toolbar: { show: false },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        barHeight: '200%',
+        borderRadius: 13,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      max: 100,
+      axisBorder: { show: false },
+      axisTicks: { show: false },
+      labels: { show: false },
+    },
+    yaxis: { labels: { show: false } },
+    grid: { show: false },
+    tooltip: { enabled: false },
+    fill: {
+      opacity: 0.3,
+    },
+    colors: [color],
+    legend: { show: false },
+    states: {
+      hover: { filter: { type: 'none' } },
+      active: { filter: { type: 'none' } },
+    },
+  };
+
+  const optionsDark = {
+    chart: {
+      type: 'bar' as const,
+      toolbar: { show: false },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        barHeight: '200%',
+        borderRadius: 13,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      max: 100,
+      axisBorder: { show: false },
+      axisTicks: { show: false },
+      labels: { show: false },
+    },
+    yaxis: { labels: { show: false } },
+    grid: { show: false },
+    tooltip: { enabled: false },
+    fill: {
+      opacity: 1,
+    },
+    colors: [color],
+    legend: { show: false },
+    states: {
+      hover: { filter: { type: 'none' } },
+      active: { filter: { type: 'none' } },
+    },
+  };
+
+  const seriesLight = [
+    {
+      data: [100],
+    },
+  ];
+
+  const seriesDark = [
+    {
+      data: [validValue],
+    },
+  ];
+
+  return (
+    <div className="flex">
+      <span className="pt-[27px]">{children}</span>
+        <div className="relative w-[300px] h-[200px] justify-center items-center">
+          <div className="absolute w-full top-0 left-0 z-1">
+              <ReactApexChart options={optionsLight} series={seriesLight} type="bar" height={60} />
+          </div>
+          <div className="absolute top-0 left-0 w-full z-2">
+              <ReactApexChart options={optionsDark} series={seriesDark} type="bar" height={60} />
+          </div>
+          <span className="absolute top-[30px] left-[150px] text-[14px]">{value}%</span>
+        </div>
+    </div>
+  );
+};
+
+export default BarChart;
