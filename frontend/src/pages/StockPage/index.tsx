55ffcd91d52af0a435bd6d10019f15ca0c206598
이상휘 <sanghwi47@naver.com>
2024-09-23 10:02:27 +0900

    Feat: StockPage에 페이지구성용 임시 차트 생성 /S11P21B201-208

--- frontend/src/pages/StockPage/index.tsx
+++ frontend/src/pages/StockPage/index.tsx
@@ -4,6 +4,8 @@ import Summary from "../../components/stock/Summary";
 import Button from "../../components/elements/Button";
 import InvestInfo from "../../components/stock/InvestInfo";
 import EnterpriseInfo from "../../components/stock/EnterpriseInfo";
+import BestAnalystList from "../../components/stock/bestAnalyst/BestAnalystList";
+import StockChart from "../../components/stock/StockChart";
 
 const StockPage = () => {
   return (
@@ -18,8 +20,12 @@ const StockPage = () => {
         </div>
       </div>
       <div className="middle">
-        <div className="chart">차트그림 가로:70% 세로:35vh</div>
-        <div>Best분석가 30%</div>
+        <div className="chart">
+          <StockChart></StockChart>
+        </div>
+        <div>
+          <BestAnalystList></BestAnalystList>
+        </div>
       </div>
       <div className="bottom">
         <div className="infos">
