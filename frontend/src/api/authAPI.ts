be0afbdeeceeb11cdd61cf0a19008030004cd32e
Taehankk <162579158+Taehankk@users.noreply.github.com>
2024-09-24 15:46:29 +0900

    feat: auth page api 연결

---
+++ frontend/src/api/authAPI.ts
@@ -0,0 +1,85 @@
+import axios from "axios";
+import axiosInstance from "./axiosInstance";
+
+export const sendEmailAPI = async (email: string) => {
+  try {
+    const res = await axiosInstance.post("/user/send-mail", {
+      userEmail: email,
+    });
+
+    alert("이메일에서 인증코드를 확인해주세요.");
+    return res.data.emailAuthId;
+  } catch (e: any) {
+    if (e.response && e.response.status === 400) {
+      alert("이미 가입한 이메일입니다.");
+      return "";
+    }
+  }
+  // console.log(e.response.status);
+  // console.log(e.response.data);
+};
+
+export const checkValidateAPI = async (
+  emailAuthId: number,
+  validateNum: string
+) => {
+  try {
+    await axiosInstance.post("/user/send-mail/validation", {
+      emailAuthId: emailAuthId,
+      randomKey: validateNum,
+    });
+
+    alert("이메일 인증 성공");
+
+    return true;
+  } catch (e: any) {
+    if (e.response) {
+      console.log(e.response);
+      alert("인증번호를 확인해주세요");
+      return false;
+    }
+  }
+};
+
+export const checkAnalystCodeAPI = async (code: string) => {
+  try {
+    await axiosInstance.post("/user/auth-code", {
+      codeNumber: code,
+    });
+
+    return true;
+  } catch (e: any) {
+    if (e.response && e.response.status === 400) {
+      alert("인증코드를 확인하세요");
+      return false;
+    }
+  }
+};
+
+export const signUpAPI = async (
+  email: string,
+  password: string,
+  name: string,
+  nickname: string,
+  role: string,
+  emailAuthId: number
+) => {
+  try {
+    await axiosInstance.post("/user/sign-up", {
+      userEmail: email, //유저 이메일
+      userPassword: password, //유저 패스워드
+      userName: name, //애널리스트 실명
+      userNickname: nickname, //일반 유저 닉네임
+      roleType: role, //역할 [유저, 애널리스트, 구독자]
+      emailAuthId: emailAuthId,
+    });
+
+    alert("회원가입이 정상적으로 완료되었습니다.");
+    return 0;
+  } catch (e: any) {
+    if (e.response) {
+      alert("입력정보를 확인해주세요");
+    }
+    return 1;
+  }
+};
---
+++ frontend/src/components/auth/SignUpContext.tsx
@@ -0,0 +1,145 @@
+import { ReactNode, createContext, useState } from "react";
+
+interface Context {
+  nicknameInput: string;
+  setNicknameInput: (value: string) => void;
+  emailInput: string;
+  setEmailInput: (value: string) => void;
+  validateNumInput: string;
+  setValidateNumInput: (value: string) => void;
+  passwordInput: string;
+  setPasswordInput: (value: string) => void;
+  pwCheckInput: string;
+  setPwCheckInput: (value: string) => void;
+  emailAuthId: number;
+  setEmailAuthId: (value: number) => void;
+
+  isAnalyst: boolean;
+  setIsAnalyst: (value: boolean) => void;
+  codeInput: string;
+  setCodeInput: (value: string) => void;
+  nameInput: string;
+  setNameInput: (value: string) => void;
+
+  isNicknameValid: boolean;
+  setNicknameValid: (value: boolean) => void;
+  isEmailValid: boolean;
+  setEmailValid: (value: boolean) => void;
+  isValidateNumValid: boolean;
+  setValidateNumValid: (value: boolean) => void;
+  isPasswordValid: boolean;
+  setPasswordValid: (value: boolean) => void;
+  isPwCheckValid: boolean;
+  setPwCheckValid: (value: boolean) => void;
+  isCodeValid: boolean;
+  setCodeValid: (value: boolean) => void;
+  isNameValid: boolean;
+  setNameValid: (value: boolean) => void;
+}
+
+const SignUpContext = createContext<Context>({
+  nicknameInput: "",
+  setNicknameInput: () => {},
+  emailInput: "",
+  setEmailInput: () => {},
+  validateNumInput: "",
+  setValidateNumInput: () => {},
+  passwordInput: "",
+  setPasswordInput: () => {},
+  pwCheckInput: "",
+  setPwCheckInput: () => {},
+  emailAuthId: 0,
+  setEmailAuthId: () => {},
+
+  isAnalyst: false,
+  setIsAnalyst: () => {},
+  codeInput: "",
+  setCodeInput: () => {},
+  nameInput: "",
+  setNameInput: () => {},
+
+  isNicknameValid: false,
+  setNicknameValid: () => {},
+  isEmailValid: false,
+  setEmailValid: () => {},
+  isValidateNumValid: false,
+  setValidateNumValid: () => {},
+  isPasswordValid: false,
+  setPasswordValid: () => {},
+  isPwCheckValid: false,
+  setPwCheckValid: () => {},
+  isCodeValid: false,
+  setCodeValid: () => {},
+  isNameValid: false,
+  setNameValid: () => {},
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+const SignUpContextProvider = ({ children }: Props) => {
+  const [nicknameInput, setNicknameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [validateNumInput, setValidateNumInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [pwCheckInput, setPwCheckInput] = useState("");
+  const [emailAuthId, setEmailAuthId] = useState(0);
+
+  const [isAnalyst, setIsAnalyst] = useState(false);
+  const [codeInput, setCodeInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
+
+  const [isNicknameValid, setNicknameValid] = useState(false);
+  const [isEmailValid, setEmailValid] = useState(false);
+  const [isValidateNumValid, setValidateNumValid] = useState(false);
+  const [isPasswordValid, setPasswordValid] = useState(false);
+  const [isPwCheckValid, setPwCheckValid] = useState(false);
+  const [isCodeValid, setCodeValid] = useState(false);
+  const [isNameValid, setNameValid] = useState(false);
+
+  return (
+    <SignUpContext.Provider
+      value={{
+        nicknameInput,
+        setNicknameInput,
+        emailInput,
+        setEmailInput,
+        validateNumInput,
+        setValidateNumInput,
+        passwordInput,
+        setPasswordInput,
+        pwCheckInput,
+        setPwCheckInput,
+        emailAuthId,
+        setEmailAuthId,
+
+        isAnalyst,
+        setIsAnalyst,
+        codeInput,
+        setCodeInput,
+        nameInput,
+        setNameInput,
+
+        isNicknameValid,
+        setNicknameValid,
+        isEmailValid,
+        setEmailValid,
+        isValidateNumValid,
+        setValidateNumValid,
+        isPasswordValid,
+        setPasswordValid,
+        isPwCheckValid,
+        setPwCheckValid,
+        isCodeValid,
+        setCodeValid,
+        isNameValid,
+        setNameValid,
+      }}
+    >
+      {children}
+    </SignUpContext.Provider>
+  );
+};
+
+export { SignUpContext, SignUpContextProvider };
