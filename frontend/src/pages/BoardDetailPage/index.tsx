93b36392420d916ef5458c1b34efc9e3eda0500b
Taehankk <162579158+Taehankk@users.noreply.github.com>
2024-09-24 13:31:50 +0900

    feat: community page 임시 push

--- frontend/src/pages/BoardDetailPage/index.tsx
+++ frontend/src/pages/BoardDetailPage/index.tsx
@@ -1,9 +1,60 @@
 import BasicLayout from "../../layouts/BasicLayout";
+import Button from "../../components/elements/Button";
+
+import Winter from "../../assets/winter.jpg";
 
 const BoardDetailPage = () => {
   return (
     <BasicLayout>
-      <div>일반회원이 작성한 게시글의 상세내용입니다</div>
+      {/* 제목 */}
+      <div className="text-xl">자유게시판 글 제목입니다.</div>
+      {/* 제목 밑 파트 */}
+      {/* 시간, 좋아요, 조회수 */}
+      <div className="text-xs w-full">
+        <div className="flex opacity-50 text-sm mt-3">
+          <span className="text-xs mr-12">#태그</span>
+          <div className="">
+            <span className="mr-2">좋아요</span>
+            <span className="mr-2">조회수</span>
+            <span>댓글수</span>
+          </div>
+        </div>
+
+        <div className="flex items-center mt-3">
+          <div className="flex mr-12">
+            <img
+              src={Winter}
+              alt="프로필사진"
+              className="w-6 h-6 object-cover border rounded-full border-black"
+            />
+          </div>
+          작성자
+          <div className="flex">
+            <div className="mr-4">
+              <span className="mr-2">작성일</span>
+              <span>24.09.08</span>
+            </div>
+            <div>
+              <span className="mr-2">수정일</span>
+              <span>24.09.12</span>
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <Button
+              size="small"
+              color="black"
+              children="수정"
+              className="bg-white text-sm w-10"
+            />
+            <Button
+              size="small"
+              color="black"
+              children="삭제"
+              className="bg-white text-sm w-10"
+            />
+          </div>
+        </div>
+      </div>
     </BasicLayout>
   );
 };
--- frontend/src/pages/ReportWritePage/index.tsx
+++ frontend/src/pages/ReportWritePage/index.tsx
@@ -1,9 +1,34 @@
+import Button from "../../components/elements/Button";
+import Input from "../../components/elements/Input";
 import BasicLayout from "../../layouts/BasicLayout";
 
 const ReportWritePage = () => {
   return (
     <BasicLayout>
-      <div>분석글작성폼입니다.</div>
+      <div className="w-full">
+        <div>
+          <span>주식종목</span>
+          <span>삼성전자</span>
+        </div>
+        <div>
+          <span>파일</span>
+          <span>종목분석 PDF를 업로드 해주세요</span>
+        </div>
+        <Input placeholder="제목을 입력해주세요" className="border-none" />
+
+        <div>글 작성 라이브러리 칸</div>
+
+        <div className="flex justify-end">
+          <Button
+            size="small"
+            color="black"
+            border="black"
+            children="취소"
+            className="bg-white"
+          />
+          <Button size="small" children="등록" />
+        </div>
+      </div>
     </BasicLayout>
   );
 };
