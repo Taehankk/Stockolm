74f94cef93fc4e4e18d1f6b94ecaf8e56ee7d6dd
최윤석 <dbstjr9898@naver.com>
2024-09-25 16:59:05 +0900

    sysout 삭제 로그로 변경

--- backend/stockolm/src/main/java/com/example/stockolm/global/config/ExecutionTimeAspect.java
+++ backend/stockolm/src/main/java/com/example/stockolm/global/config/ExecutionTimeAspect.java
@@ -3,10 +3,12 @@ package com.example.stockolm.global.config;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class ExecutionTimeAspect {
 
     @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping) || @within(org.springframework.web.bind.annotation.RestController)")
@@ -18,7 +20,7 @@ public class ExecutionTimeAspect {
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
 
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
         return proceed;
     }
 }
