fc9100ff8476bbaa31a7da631025ddcba4fb8d6d
이규빈 <kyubin.dev@gmail.com>
2024-09-26 14:20:55 +0900

    feat: 종목게시판 글 생성 기능 구현

--- backend/stockolm/src/main/java/com/example/stockolm/domain/analystBoard/service/AnalystBoardServiceImpl.java
+++ backend/stockolm/src/main/java/com/example/stockolm/domain/analystBoard/service/AnalystBoardServiceImpl.java
@@ -1,7 +1,16 @@
 package com.example.stockolm.domain.analystBoard.service;
 
+import com.example.stockolm.domain.analystBoard.dto.request.CreateAnalystBoardRequest;
 import com.example.stockolm.domain.analystBoard.dto.response.AnalystBoardResponse;
+import com.example.stockolm.domain.analystBoard.entity.AnalystBoard;
+import com.example.stockolm.domain.analystBoard.entity.GoalCategory;
 import com.example.stockolm.domain.analystBoard.repository.AnalystBoardRepository;
+import com.example.stockolm.domain.stock.entity.Stock;
+import com.example.stockolm.domain.stock.repository.StockRepository;
+import com.example.stockolm.domain.user.entity.User;
+import com.example.stockolm.domain.user.repository.UserRepository;
+import com.example.stockolm.global.exception.custom.StockNotFoundException;
+import com.example.stockolm.global.exception.custom.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +23,8 @@ import java.util.List;
 public class AnalystBoardServiceImpl implements AnalystBoardService {
 
     private final AnalystBoardRepository analystBoardRepository;
+    private final UserRepository userRepository;
+    private final StockRepository stockRepository;
 
     @Override
     public List<AnalystBoardResponse> getLikedAnalystBoard(Long userId, String stockName) {
@@ -25,5 +36,34 @@ public class AnalystBoardServiceImpl implements AnalystBoardService {
     public void setMainContent(Long userId, Long analystBoardId) {
 
         analystBoardRepository.choseMainContent(userId, analystBoardId);
+    }
+
+    @Override
+    public void createAnalystBoard(Long userId, CreateAnalystBoardRequest createAnalystBoardRequest) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        String stockName = createAnalystBoardRequest.getStockName();
+        Stock stock = stockRepository.findByStockName(stockName);
+        if (stock == null) {
+            throw new StockNotFoundException();
+        }
+
+        // file을 S3에 올리고 주소 받는 로직 추가예정
+
+        AnalystBoard analystBoard = AnalystBoard.builder()
+                .user(user)
+                .stock(stock)
+                .title(createAnalystBoardRequest.getTitle())
+                .opinion(createAnalystBoardRequest.getOpinion())
+                .goalStock(createAnalystBoardRequest.getGoalStock())
+                .currentStock(createAnalystBoardRequest.getCurrentStock())
+                .marketCapitalization(createAnalystBoardRequest.getMarketCapitalization())
+                .content(createAnalystBoardRequest.getContent())
+                .goalDate(createAnalystBoardRequest.getGoalDate())
+                .goalSuccess(GoalCategory.WAIT) // 초기값은 "대기"
+//                .filePath(filepath)
+                .build();
+
+        analystBoardRepository.save(analystBoard);
     }
 }
