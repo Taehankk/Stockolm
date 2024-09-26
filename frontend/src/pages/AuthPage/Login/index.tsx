b0d06fe0249bc70a4c02b13c368a1b0f5427fc6e
Taehankk <162579158+Taehankk@users.noreply.github.com>
2024-09-26 17:16:34 +0900

    feat: community 작성 페이지 1차 중간 개발

--- frontend/src/pages/AuthPage/Login/index.tsx
+++ frontend/src/pages/AuthPage/Login/index.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 import Button from "../../../components/elements/Button";
 import Input from "../../../components/elements/Input";
-import axios from "axios";
 import axiosInstance from "../../../api/axiosInstance";
 
 interface Props {
