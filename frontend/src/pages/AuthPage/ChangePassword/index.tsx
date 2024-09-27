f6e444be905f1d8da1e1bd5179ac29ff2fccae1f
Taehankk <162579158+Taehankk@users.noreply.github.com>
2024-09-27 13:44:31 +0900

    refactor: 이메일인증 컴포넌트 분리, 닉네임 검증 로직 분리

--- frontend/src/pages/AuthPage/ChangePassword/index.tsx
+++ frontend/src/pages/AuthPage/ChangePassword/index.tsx
@@ -1,63 +1,22 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 
 import Button from "../../../components/elements/Button";
 import Input from "../../../components/elements/Input";
 
 import {
-  validateEmail,
   validateMatchPassword,
   validatePassword,
 } from "../../../utils/validation";
 
-import { sendEmailAPI, checkValidateAPI } from "../../../api/authAPI";
-import { SignUpContext } from "../../../components/auth/SignUpContext";
+import EmailVerification from "../../../components/auth/common/EmailVerification";
 
 interface Props {
   handleImgLocation: (value: number) => void;
 }
 
 const ChangePassword = ({ handleImgLocation }: Props) => {
-  const [emailInput, setEmailInput] = useState("");
-  const [validateNumInput, setValidateNumInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
   const [pwCheckInput, setPwCheckInput] = useState("");
-
-  const { emailAuthId, setEmailAuthId, setValidateNumValid } =
-    useContext(SignUpContext);
-
-  // useEffect();
-
-  const handleEmailInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setEmailInput(value);
-  };
-
-  const handleValidateNumInputChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const value = e.target.value;
-    setValidateNumInput(value);
-  };
-
-  const sendEmail = async () => {
-    const authID = await sendEmailAPI(emailInput);
-    setEmailAuthId(authID);
-  };
-
-  const checkValidateNumber = async () => {
-    if (!emailAuthId) {
-      alert("이메일 인증이 필요합니다.");
-      return;
-    }
-
-    const valid = await checkValidateAPI(emailAuthId, validateNumInput);
-
-    if (valid) {
-      setValidateNumValid(true);
-    } else {
-      setValidateNumValid(false);
-    }
-  };
 
   const handlePasswordInputChange = (
     e: React.ChangeEvent<HTMLInputElement>
@@ -82,37 +41,7 @@ const ChangePassword = ({ handleImgLocation }: Props) => {
 
       {/* 이메일 input */}
       <div className="flex-col justify-between">
-        <div className="flex mb-2 items-center justify-between">
-          <span className="">이메일</span>
-          <Input
-            onChange={handleEmailInputChange}
-            value={emailInput}
-            validate={validateEmail}
-            type="password"
-          />
-        </div>
-        <div className="flex justify-end">
-          <Button
-            onClick={sendEmail}
-            children="이메일 인증"
-            className="mb-2 text-[0.85rem]"
-          />
-        </div>
-        <div className="flex items-center justify-end mb-2">
-          <Input
-            size="small"
-            placeholder="인증 코드 입력"
-            onChange={handleValidateNumInputChange}
-            value={validateNumInput}
-            className="mr-8 w-32 text-sm"
-          />
-          <Button
-            size="small"
-            onClick={checkValidateNumber}
-            children="확인"
-            className="text-[0.8rem] w-20"
-          />
-        </div>
+        <EmailVerification />
         {/* 새 비밀번호 input */}
         <div className="flex mb-2 items-center justify-between">
           <span className="">새 비밀번호</span>
