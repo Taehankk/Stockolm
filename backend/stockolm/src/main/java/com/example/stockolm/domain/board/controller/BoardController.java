18d68be7aa80fe09336b8c40fbeb513818d08fcf
이규빈 <kyubin.dev@gmail.com>
2024-09-30 15:33:46 +0900

    feat: 자유게시판 글 목록 조회 기능 구현

--- backend/stockolm/src/main/java/com/example/stockolm/domain/board/controller/BoardController.java
+++ backend/stockolm/src/main/java/com/example/stockolm/domain/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.stockolm.domain.board.controller;
 import com.example.stockolm.domain.board.dto.request.CreateBoardRequest;
 import com.example.stockolm.domain.board.dto.request.CreateCommentRequest;
 import com.example.stockolm.domain.board.dto.request.ModifyBoardRequest;
+import com.example.stockolm.domain.board.dto.response.BoardPageResponse;
 import com.example.stockolm.domain.board.dto.response.BoardResponse;
 import com.example.stockolm.domain.board.service.BoardService;
 import com.example.stockolm.global.auth.AuthPrincipal;
@@ -11,6 +12,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +35,16 @@ public class BoardController {
         if (userId == null) {
             throw new LoginRequiredException();
         }
+    }
+
+    @GetMapping
+    @Operation(summary = "글 목록 조회", description = "자유게시판 글 목록 조회 API")
+    public ResponseEntity<Page<BoardPageResponse>> getBoardPage(@RequestParam(required = false) String searchWord,
+                                                                @PageableDefault(sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable,
+                                                                @AuthPrincipal @Parameter(hidden = true) Long userId) {
+        // 로그인 없이 글 목록 조회 가능
+        Page<BoardPageResponse> boardPage = boardService.getBoardPage(searchWord, pageable, userId);
+        return ResponseEntity.status(OK).body(boardPage);
     }
 
     @PostMapping
@@ -58,9 +73,9 @@ public class BoardController {
 
     @GetMapping("/{boardId}")
     @Operation(summary = "글 상세 조회", description = "자유게시판 글 상세 조회 API")
-    public ResponseEntity<BoardResponse> retrieveBoard(@PathVariable Long boardId, @AuthPrincipal @Parameter(hidden = true) Long userId) {
+    public ResponseEntity<BoardResponse> getBoard(@PathVariable Long boardId, @AuthPrincipal @Parameter(hidden = true) Long userId) {
         // 로그인 없이 글 조회 가능
-        BoardResponse boardResponse = boardService.retrieveBoard(boardId, userId);
+        BoardResponse boardResponse = boardService.getBoard(boardId, userId);
         return ResponseEntity.status(OK).body(boardResponse);
     }
 
@@ -79,4 +94,5 @@ public class BoardController {
         boardService.createComment(boardId, userId, createCommentRequest);
         return ResponseEntity.status(CREATED).build();
     }
+
 }
--- backend/stockolm/src/main/java/com/example/stockolm/domain/board/service/BoardService.java
+++ backend/stockolm/src/main/java/com/example/stockolm/domain/board/service/BoardService.java
@@ -3,16 +3,21 @@ package com.example.stockolm.domain.board.service;
 import com.example.stockolm.domain.board.dto.request.CreateBoardRequest;
 import com.example.stockolm.domain.board.dto.request.CreateCommentRequest;
 import com.example.stockolm.domain.board.dto.request.ModifyBoardRequest;
+import com.example.stockolm.domain.board.dto.response.BoardPageResponse;
 import com.example.stockolm.domain.board.dto.response.BoardResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BoardService {
+    Page<BoardPageResponse> getBoardPage(String searchWord, Pageable pageable, Long userId);
+
     void createBoard(Long userId, CreateBoardRequest createBoardRequest);
 
     void modifyBoard(Long boardId, Long userId, ModifyBoardRequest modifyBoardRequest);
 
     void removeBoard(Long boardId, Long userId);
 
-    BoardResponse retrieveBoard(Long boardId, Long userId);
+    BoardResponse getBoard(Long boardId, Long userId);
 
     void likeBoard(Long boardId, Long userId);
 
