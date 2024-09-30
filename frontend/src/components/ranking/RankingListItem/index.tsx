3d98bbd0fc994e174ae9ddd02272104b4224f39e
이상휘 <sanghwi47@naver.com>
2024-09-30 22:46:30 +0900

    Feat: 실제 data에 맞추어 rankingPage 구현 /S11P21B201-276

--- frontend/src/components/ranking/RankingListItem/index.tsx
+++ frontend/src/components/ranking/RankingListItem/index.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 
 interface RankingListItemProps {
-  ranking: number;
-  analystName: string;
-  postCount: number;
-  totalScore: number;
+  userName: string;
+  userNickname: string;
+  userImagePath: string;
+  totalAnalystRanking: number;
+  totalBoardSize: number;
+  reliability: number;
+  accuracy: number;
 }
 
 const RankingListItem: React.FC<RankingListItemProps> = ({
-  ranking,
-  analystName,
-  postCount,
-  totalScore,
+  userName,
+  userImagePath,
+  totalAnalystRanking,
+  totalBoardSize,
+  reliability,
+  accuracy,
 }) => {
   return (
     <div className="flex justify-around items-center w-full h-[5rem]">
       <span className="w-1/6 text-center">
-        <span>랭킹이미지 </span>
-        <span>{ranking}.</span>
+        <span>{totalAnalystRanking}.</span>
       </span>
       <span className="w-1/6 text-center">
-        <span>분석가이미지 </span>
-        <span>{analystName}</span>
+        <img
+          src={userImagePath}
+          alt={`${userName}'s profile`}
+          className="w-2 h-2 rounded-full"
+        />
+        <span>{userName}</span>
       </span>
-      <span className="w-1/6 text-center">{postCount}건</span>
-      <span className="w-1/6 text-center">{totalScore}%</span>
+      <span className="w-1/6 text-center">{totalBoardSize}건</span>
+      <span className="w-1/6 text-center">
+        {Math.round((reliability + accuracy) / 2)}%
+      </span>
     </div>
   );
 };
