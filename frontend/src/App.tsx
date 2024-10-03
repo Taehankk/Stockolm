c83d65a7e39bdd88366d7ee9701d39e4e4d080b3
김민호 <oilpott12@naver.com>
2024-10-03 18:01:10 +0900

    마이페이지 api 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,16 @@ import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import root from "./router/root";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={root} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={root} />
+      </QueryClientProvider>
     </Provider>
   );
 }
