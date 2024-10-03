965b42800765155ca3f1fd5674e3c3bd06dbe3dd
이상휘 <sanghwi47@naver.com>
2024-10-03 14:29:04 +0900

    Feat: BestAnalyst Llst 출력 /S11P21B201-278

--- frontend/src/components/stock/bestAnalyst/BestAnalystListItem/index.tsx
+++ frontend/src/components/stock/bestAnalyst/BestAnalystListItem/index.tsx
@@ -1,26 +1,43 @@
 import face from "../../../../assets/face.jpg";
+import { Analyst } from "../../../../types/stock";
+import { useNavigate } from "react-router-dom";
 
-const BestAnalystListItem = () => {
+interface BestAnalystListItemProps {
+  analyst: Analyst;
+}
+
+const BestAnalystListItem = ({ analyst }: BestAnalystListItemProps) => {
+  const nav = useNavigate();
+
+  const handleBestAnalystClick = () => {
+    nav(`/analyst/${analyst.analystName}/report/${analyst.analystBoardId}`);
+  };
+
   return (
-    <div className="bg-white flex px-2 py-1 border-t border-b border-gray-200 ">
+    <div
+      className="bg-white flex px-2 py-1 border-t border-b border-gray-200 cursor-pointer hover:bg-slate-100"
+      onClick={handleBestAnalystClick}
+    >
       <div className="flex items-center">
         <div className="flex-1">
           <img
             className="w-[50px] h-[50px] rounded-full border"
-            src={face}
-          ></img>
+            src={analyst.userImagePath || face}
+            alt={analyst.analystName}
+          />
         </div>
         <div className="flex-col flex-4 mx-5">
-          <span className="block font-normal">김싸피</span>
-          <span className="text-gray-400">신뢰도 99%</span>
+          <span className="block font-normal">{analyst.analystName}</span>
+          <span className="text-gray-400">신뢰도 {analyst.reliability}%</span>
         </div>
         <div className="flex-2">
           <span className="px-2 py-1 bg-red-400 rounded-lg text-white text-small">
-            매수추천
+            {analyst.opinion}
           </span>
         </div>
       </div>
     </div>
   );
 };
+
 export default BestAnalystListItem;
