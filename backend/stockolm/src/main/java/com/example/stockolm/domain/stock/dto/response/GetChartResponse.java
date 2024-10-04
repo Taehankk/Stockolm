251110a67abb261fa8680c5fd11f5ef60bb208ea
하상진 <lavegas1@naver.com>
2024-10-04 11:13:07 +0900

    refactor: 분봉차트 api 수정

--- backend/stockolm/src/main/java/com/example/stockolm/domain/stock/dto/response/GetChartResponse.java
+++ backend/stockolm/src/main/java/com/example/stockolm/domain/stock/dto/response/GetChartResponse.java
@@ -6,28 +6,28 @@ import lombok.Getter;
 @Getter
 public class GetChartResponse {
 
-    private final String stck_bsop_date;
+    private final String stckBsopDate;
 
-    private final String stck_cntg_hour;
+    private final String stckCntgHour;
 
-    private final String acml_tr_pbmn;
+    private final String acmlTrPbmn;
 
-    private final String stck_prpr;
+    private final String stckPrpr;
 
-    private final String stck_oprc;
+    private final String stckOprc;
 
-    private final String stck_hgpr;
+    private final String stckHgpr;
 
-    private final String stck_lwpr;
+    private final String stckLwpr;
 
     @Builder
-    public GetChartResponse(String stck_bsop_date, String stck_cntg_hour, String acml_tr_pbmn, String stck_prpr, String stck_oprc, String stck_hgpr, String stck_lwpr) {
-        this.stck_bsop_date = stck_bsop_date;
-        this.stck_cntg_hour = stck_cntg_hour;
-        this.acml_tr_pbmn = acml_tr_pbmn;
-        this.stck_prpr = stck_prpr;
-        this.stck_oprc = stck_oprc;
-        this.stck_hgpr = stck_hgpr;
-        this.stck_lwpr = stck_lwpr;
+    public GetChartResponse(String stckBsopDate, String stckCntgHour, String acmlTrPbmn, String stckPrpr, String stckOprc, String stckHgpr, String stckLwpr) {
+        this.stckBsopDate = stckBsopDate;
+        this.stckCntgHour = stckCntgHour;
+        this.acmlTrPbmn = acmlTrPbmn;
+        this.stckPrpr = stckPrpr;
+        this.stckOprc = stckOprc;
+        this.stckHgpr = stckHgpr;
+        this.stckLwpr = stckLwpr;
     }
 }
