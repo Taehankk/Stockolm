e301b838730d7c1b32dffa74995c77e69eabf7f6
Yungsix <dbstjr9898@naver.com>
2024-10-07 16:27:12 +0900

    fix: 랭킹 조회 응답시 점수 응답, 신뢰도, 정확도 별 정렬 수정

--- backend/stockolm/src/main/java/com/example/stockolm/domain/rank/dto/response/AnalystRankInfoResponse.java
+++ backend/stockolm/src/main/java/com/example/stockolm/domain/rank/dto/response/AnalystRankInfoResponse.java
@@ -12,19 +12,21 @@ public class AnalystRankInfoResponse {
     private String userImagePath;
     private int totalAnalystRanking;
     private Long totalBoardSize;
+    private int totalAnalystScore;
     private Double reliability;
     private Double accuracy;
 
     @Builder
     public AnalystRankInfoResponse(String userName, String userNickname,
                                    String userImagePath, int totalAnalystRanking,
-                                   Long totalBoardSize, Double reliability,
-                                   Double accuracy) {
+                                   Long totalBoardSize, int totalAnalystScore,
+                                   Double reliability, Double accuracy) {
         this.userName = userName;
         this.userNickname = userNickname;
         this.userImagePath = userImagePath;
         this.totalAnalystRanking = totalAnalystRanking;
         this.totalBoardSize = totalBoardSize;
+        this.totalAnalystScore = totalAnalystScore;
         this.reliability = reliability;
         this.accuracy = accuracy;
     }
