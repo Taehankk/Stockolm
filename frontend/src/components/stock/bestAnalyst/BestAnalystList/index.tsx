bc3ca505f9c661d2cefaeb35dadef902d6cd2c0f
이상휘 <sanghwi47@naver.com>
2024-10-07 15:55:39 +0900

    Style: 주식페이지 css 수정 /S11P21B201-320

--- frontend/src/components/stock/bestAnalyst/BestAnalystList/index.tsx
+++ frontend/src/components/stock/bestAnalyst/BestAnalystList/index.tsx
@@ -9,8 +9,10 @@ const BestAnalystList = () => {
   );
   if (!bestAnalysts || bestAnalysts.length === 0) {
     return (
-      <div className="flex-col px-5 py-2 w-[23vw] h-[33vh] border border-b-1 rounded-2xl scrollbar-hide">
-        <span className="flex justify-center text-xl mb-2 ">Best 분석가</span>
+      <div className="flex-col px-5 py-2 w-[25vw] h-[33vh] border border-b-1 rounded-2xl scrollbar-hide ml-10">
+        <span className="flex justify-center text-2xl mx-2 py-3 border-b-2 border-opacity-50 border-b-slate-700 ">
+          Best 분석가
+        </span>
         <div className="flex justify-center items-center text-gray-500 mt-20">
           해당 종목에 대한 분석자료가 없습니다
         </div>
@@ -19,8 +21,10 @@ const BestAnalystList = () => {
   }
 
   return (
-    <div className="flex-col px-5 py-2 w-[23vw] h-[33vh] border border-b-1 rounded-2xl scrollbar-hide">
-      <span className="flex justify-center text-xl mb-2 ">Best 분석가</span>
+    <div className="flex-col px-5 py-2 w-[25vw] h-[50vh] border rounded-2xl overflow-y-scroll scrollbar-hide ml-10">
+      <span className="flex justify-center text-2xl mx-2 py-3 border-b-2 border-opacity-50 border-b-slate-700">
+        Best 분석가
+      </span>
       <div>
         {bestAnalysts.map((analyst, index) => (
           <BestAnalystListItem key={index} analyst={analyst} />
