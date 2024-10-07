d36d0e8ca1f8bfc6160b920ecfe1eed6e2fabb6d
이상휘 <sanghwi47@naver.com>
2024-10-07 14:08:33 +0900

    Style: Ranking 페이지 css 수정 /S11P21B201-321

--- frontend/src/components/ranking/RankingListItem/index.tsx
+++ frontend/src/components/ranking/RankingListItem/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Rank1 from "../../../assets/Rank1.svg";
+import Rank2 from "../../../assets/Rank2.svg";
+import Rank3 from "../../../assets/Rank3.svg";
 
 interface RankingListItemProps {
   userName: string;
@@ -26,21 +29,38 @@ const RankingListItem: React.FC<RankingListItemProps> = ({
     nav(`/analyst/${userNickname}`);
   };
 
+  const getRankImage = () => {
+    if (totalAnalystRanking === 1) return Rank1;
+    if (totalAnalystRanking === 2) return Rank2;
+    if (totalAnalystRanking === 3) return Rank3;
+  };
+
   return (
     <div
       className="flex justify-around items-center w-full h-[5rem] cursor-pointer hover:bg-slate-200"
       onClick={handleClick}
     >
       <span className="w-1/6 text-center">
-        <span>{totalAnalystRanking}.</span>
+        {getRankImage() ? (
+          <div className="flex justify-center mr-7">
+            <img
+              src={getRankImage()}
+              alt={`Rank ${totalAnalystRanking}`}
+              className="w-6 h-6 mr-2 "
+            />
+            <span className="mt-[0.17rem]">{totalAnalystRanking}.</span>
+          </div>
+        ) : (
+          <span>{totalAnalystRanking}.</span>
+        )}
       </span>
-      <span className="w-1/6 text-center">
+      <span className="w-1/6 flex text-center items-center justify-center gap-3">
         <img
           src={userImagePath}
           alt={`${userName}'s profile`}
-          className="w-2 h-2 rounded-full"
+          className="w-11 h-11 rounded-full"
         />
-        <span>{userName}</span>
+        <span className="items-center justify-center">{userName}</span>
       </span>
       <span className="w-1/6 text-center">{totalBoardSize}건</span>
       <span className="w-1/6 text-center">
