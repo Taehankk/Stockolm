4e673ed6294cebe9070e8d77121a77d5142b0ec3
Taehankk <162579158+Taehankk@users.noreply.github.com>
2024-10-09 23:26:28 +0900

    fix: board write 페이지 수정

--- frontend/src/components/boardWrite/Category/index.tsx
+++ frontend/src/components/boardWrite/Category/index.tsx
@@ -11,21 +11,21 @@ const Category = ({ category, handleCategory }: Props) => {
         <div
           key="WORRY"
           onClick={() => handleCategory("WORRY")}
-          className={`border ${category === "WORRY" ? "border-black" : "border-opacity-50"} rounded-md w-[6rem] h-[2rem] text-center content-center`}
+          className={`cursor-pointer border ${category === "WORRY" ? "border-black" : "border-opacity-50"} rounded-md w-[6rem] h-[2rem] text-center content-center`}
         >
           고민상담
         </div>
         <div
           key="CHAT"
           onClick={() => handleCategory("CHAT")}
-          className={`border ${category === "CHAT" ? "border-black" : "border-opacity-50"} rounded-md w-[6rem] h-[2rem] text-center content-center`}
+          className={`cursor-pointer border ${category === "CHAT" ? "border-black" : "border-opacity-50"} rounded-md w-[6rem] h-[2rem] text-center content-center`}
         >
           잡담
         </div>
         <div
           key="ETC"
           onClick={() => handleCategory("ETC")}
-          className={`border ${category === "ETC" ? "border-black" : "border-opacity-50"} rounded-md w-[6rem] h-[2rem] text-center content-center`}
+          className={`cursor-pointer border ${category === "ETC" ? "border-black" : "border-opacity-50"} rounded-md w-[6rem] h-[2rem] text-center content-center`}
         >
           기타
         </div>
