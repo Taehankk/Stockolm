64cdbe191a315b5d7c343a1c92671750330a42d6
이상휘 <sanghwi47@naver.com>
2024-10-09 17:54:21 +0900

    Feat: RankingPage 툴팁 추가/ 요소에 맞는 점수 출력

--- frontend/src/pages/RankingPage/index.tsx
+++ frontend/src/pages/RankingPage/index.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState, useAppDispatch } from "../../store";
 import { getRankings } from "../../slices/rankingSlice";
@@ -8,6 +7,7 @@ import RankingList from "../../components/ranking/RankingList";
 import RankingSearch from "../../components/ranking/RankingSearch";
 import Pagination from "../../components/common/Pagination";
 import Button from "../../components/elements/Button";
+import RedUp from "../../assets/RedUp.webp";
 
 const RankingPage: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -16,13 +16,15 @@ const RankingPage: React.FC = () => {
   );
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [rankValue, setRankValue] = useState<string | null>(null);
+  const [rankValue, setRankValue] = useState<
+    "accuracy" | "reliability" | "total" | null
+  >("total");
   const [searchQuery, setSearchQuery] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     dispatch(
       getRankings({
-        rankValue,
+        rankValue: rankValue === "total" ? null : rankValue,
         page: currentPage,
         size: 5,
         sort: null,
@@ -36,11 +38,12 @@ const RankingPage: React.FC = () => {
   };
 
   const handleSearch = (query: string) => {
+    setCurrentPage(1);
     setSearchQuery(query);
   };
 
   const handleShowAll = () => {
-    setSearchQuery(undefined); // 검색어 초기화
+    setSearchQuery(undefined);
   };
 
   const getRankText = () => {
@@ -53,60 +56,95 @@ const RankingPage: React.FC = () => {
     <BasicLayout>
       <div>
         <div className="flex-col w-4/5 m-auto">
-          <div className="flex m-auto justify-center mt-5">
-            <RankingSearch onSearch={handleSearch} />
+          <div className="flex items-center">
+            <div className="flex w-5/6 h-1/2 justify-center mt-5 ml-24">
+              <RankingSearch onSearch={handleSearch} />
+            </div>
+            <div className="relative mr-24">
+              <img
+                src={RedUp}
+                alt={`투자상승이`}
+                className="size-[8rem] ml-[3rem] mt-3 rounded-full"
+              />
+              <div className="tooltip absolute left-1/2 transform -translate-x-1/3 mt-2 px-6 bg-gray-800 text-white text-lg rounded-lg py-2 px-4 opacity-0 hover:opacity-100 transition-opacity duration-300">
+                <span>최고의 애널리스트를 만나보세요!</span>
+                <br />
+                <span>
+                  신뢰도와 정확도를 바탕으로 저희 애널리스트를 소개합니다!
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-end my-3 gap-3">
-            <Button
-              size="large"
-              fontSize="medium"
-              color="black"
-              className="bg-white mr-[35rem] py-2 flex items-center justify-center"
-              border="1"
-              onClick={handleShowAll}
-            >
-              전체순위보기
-            </Button>
-            <Button
-              size="small"
-              fontSize="medium"
-              color="black"
-              border="1"
-              className={
-                rankValue === null ? "bg-white border-slate-700" : "bg-white"
-              }
-              onClick={() => setRankValue(null)}
-            >
-              종합
-            </Button>
-            <Button
-              size="small"
-              fontSize="medium"
-              color="black"
-              border="1"
-              className={
-                rankValue === "accuracy"
-                  ? "bg-white border-slate-700"
-                  : "bg-white"
-              }
-              onClick={() => setRankValue("accuracy")}
-            >
-              정확도
-            </Button>
-            <Button
-              size="small"
-              fontSize="medium"
-              color="black"
-              border="1"
-              className={
-                rankValue === "reliability"
-                  ? "bg-white border-slate-700"
-                  : "bg-white"
-              }
-              onClick={() => setRankValue("reliability")}
-            >
-              신뢰도
-            </Button>
+          <div className="flex justify-end my-3 gap-3 relative">
+            <div className="relative group">
+              <Button
+                size="large"
+                fontSize="medium"
+                color="black"
+                className="bg-white mr-[35rem] py-2 flex items-center justify-center"
+                border="1"
+                onClick={handleShowAll}
+              >
+                전체순위보기
+              </Button>
+            </div>
+            <div className="relative group">
+              <Button
+                size="small"
+                fontSize="medium"
+                color="black"
+                border="1"
+                className={
+                  rankValue === "total"
+                    ? "bg-white border-slate-700"
+                    : "bg-white"
+                }
+                onClick={() => setRankValue("total")}
+              >
+                종합
+              </Button>
+              <div className="tooltip-content bg-gray-700 min-w-[23rem] text-white text-sm rounded-lg py-2 px-3 absolute top-full mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+                종합 점수는 정확도와 신뢰도를 반영한 Rating이에요!
+              </div>
+            </div>
+            <div className="relative group">
+              <Button
+                size="small"
+                fontSize="medium"
+                color="black"
+                border="1"
+                className={
+                  rankValue === "accuracy"
+                    ? "bg-white border-slate-700"
+                    : "bg-white"
+                }
+                onClick={() => setRankValue("accuracy")}
+              >
+                정확도
+              </Button>
+              <div className="tooltip-content bg-gray-700 min-w-[23rem] text-white text-sm rounded-lg py-2 px-3 absolute top-full mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+                예측값과 실제값 사이에 수치차이를 기반으로 산출돼요!
+              </div>
+            </div>
+            <div className="relative group">
+              <Button
+                size="small"
+                fontSize="medium"
+                color="black"
+                border="1"
+                className={
+                  rankValue === "reliability"
+                    ? "bg-white border-slate-700"
+                    : "bg-white"
+                }
+                onClick={() => setRankValue("reliability")}
+              >
+                신뢰도
+              </Button>
+              <div className="tooltip-content bg-gray-700 min-w-[15rem] whitespace-nowrap text-white text-sm rounded-lg py-2 px-3 absolute top-full mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+                상승과 하락에 대한 예측 신뢰도에요!
+              </div>
+            </div>
           </div>
           <div className="flex justify-around items-center w-full h-[4rem] border-b-black border-b-[0.1rem]">
             <span className="w-1/6 text-center">순위</span>
@@ -115,7 +153,7 @@ const RankingPage: React.FC = () => {
             <span className="w-1/6 text-center">{getRankText()}</span>
           </div>
           <div className="h-3/5">
-            <RankingList items={rankings} />
+            <RankingList items={rankings} rankValue={rankValue} />
           </div>
           <Pagination
             currentPage={currentPage}
